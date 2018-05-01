c723105f224d935ec7655de217f729fabde01706
kennyenokian <kenny.enokian@gmail.com>
2018-05-01 12:56:14 -0500

    Update header.php
    fixed home button so it isn't just a back button

--- header.php
+++ header.php
@@ -2,7 +2,9 @@
         
         <div class="col text-left">
              
-            <button class = "btn btn-dark align-right" ng-disabled="home" onClick = "history.back()">Home</button>
+            <!--<button class = "btn btn-dark align-right" ng-disabled="home" onClick = "history.back()">Back</button>-->
+            <button class = "btn btn-dark align-right" ng-click ="goHome('<?php session_start(); $userRole = $_SESSION['role']; echo "$userRole";?>')"
+            ng-disabled ="home">Home</button>
         </div>
         <div class="col text-right">
             <button class="btn btn-dark align-right" ng-click="logout()">Logout, <?php
