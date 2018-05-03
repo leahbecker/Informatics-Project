282d19152bd6f467330ba5f4fabe45b96f3e3236
leahbecker <38045616+leahbecker@users.noreply.github.com>
2018-05-02 20:24:36 -0500

    Update tutorapp.php

--- tutorapp.php
+++ tutorapp.php
@@ -7,36 +7,28 @@ include_once('dbutils.php');
 // get a connection to the database
 $db = connectDB($DBHost, $DBUser, $DBPassword, $DBName);
 // $user = $_SESSION['username'];
-$tablename = "courseTaken";
+$tablename = "tutorApp";
 //*******change this to only get slots that do not have an assigned student
 //(WHERE FK_student = null)
-$query = "SELECT courseTakenID, FFK_hawkID, email, phone, courseNum, courseName, courseGrade FROM $tablename;";
-
+$query = "SELECT FK_hawkID, email, phone FROM $tablename;";
 $result = queryDB($query, $db);
-
-$courseTakenArray = array();
+$tutorAppArray = array();
 $i = 0;
-
 // go through the results one by one
-while ($currCourse = nextTuple($result)) {
-    $courseTakenArray[$i] = $currCourse;
+while ($currApp = nextTuple($result)) {
+    $tutorAppArray[$i] = $currApp;
     $i++;
 }
-
-
 // put together a JSON object
 $response = array();
 //$response2 = array();
 $response['status'] = 'success';
 //$response['value']['user'] = $_Session['username'];
 //$response2['status'] = 'success';
-$response['value']['courseTaken'] = $courseTakenArray;
+$response['value']['tutorApp'] = $tutorAppArray;
 //$response['value']['user'] = $user;
 header('Content-Type: application/json');
 echo(json_encode($response));
-
-
-
 //echo(json_encode($response2));
 /* 
     include_once('config.php');
